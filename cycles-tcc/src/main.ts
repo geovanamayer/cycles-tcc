c0625cb469c5029575e51c9a8642b13cd0d677f7
Geovana Nogueira Mayer <99758327+geovanamayer@users.noreply.github.com>
2024-06-17 15:43:42 -0300

    Add files via upload

--- cycles-tcc/src/main.ts
+++ cycles-tcc/src/main.ts
@@ -1,20 +1,6 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
-import firebase from 'firebase/compat/app'; // Importe apenas o módulo 'app' do Firebase
+import { bootstrapApplication } from '@angular/platform-browser';
+import { appConfig } from './app/app.config';
+import { AppComponent } from './app/app.component';
 
-// Configure o Firebase com sua configuração
-const firebaseConfig = {
-  apiKey: "AIzaSyDyR0Hj_oAAZY2st1a0hZEFl_bfV8O9bhk",
-  authDomain: "tcc-cycles-a6cca.firebaseapp.com",
-  projectId: "tcc-cycles-a6cca",
-  storageBucket: "tcc-cycles-a6cca.appspot.com",
-  messagingSenderId: "867426805367",
-  appId: "1:867426805367:web:12ef396001d700577e8872",
-  measurementId: "G-7770K57SFG"
-
-};
-
-firebase.initializeApp(firebaseConfig);
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+bootstrapApplication(AppComponent, appConfig)
+  .catch((err) => console.error(err));
