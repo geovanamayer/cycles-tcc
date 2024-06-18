8a7aadf804d3fa0697cd4d46e0448aa1da52fd34
Geovana Nogueira Mayer <99758327+geovanamayer@users.noreply.github.com>
2024-06-18 01:52:00 -0300

    Add files via upload

--- cycles-tcc/src/main.server.ts
+++ cycles-tcc/src/main.server.ts
@@ -1,56 +1,7 @@
-import express from 'express';
-import { json } from 'body-parser'; // Use body-parser instead of json-server's json
-import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine } from '@angular/ssr';
-import { fileURLToPath } from 'url';
-import { dirname, join, resolve } from 'path';
-import bootstrap from './src/main.server';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { AppComponent } from './app/app.component';
+import { config } from './app/app.config.server';
 
-export function app(): express.Express {
-  const server = express();
-  const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-  const browserDistFolder = resolve(serverDistFolder, '../browser');
-  const indexHtml = join(serverDistFolder, 'index.server.html');
-  const commonEngine = new CommonEngine();
+const bootstrap = () => bootstrapApplication(AppComponent, config);
 
-  server.set('view engine', 'html');
-  server.set('views', browserDistFolder);
-
-  server.use('/api', json()); // Use body-parser middleware
-
-  // Serve static files from /browser
-  server.get('*.*', express.static(browserDistFolder, {
-    maxAge: '1y'
-  }));
-
-  // All regular routes use the Angular engine
-  server.get('*', (req, res, next) => {
-    const { protocol, headers } = req;
-    const baseUrl = ''; // Define base URL as needed
-
-    commonEngine
-      .render({
-        bootstrap,
-        documentFilePath: indexHtml,
-        url: `${protocol}://${headers.host}${req.originalUrl}`,
-        publicPath: browserDistFolder,
-        providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-      })
-      .then((html) => res.send(html))
-      .catch((err) => next(err));
-  });
-
-  return server;
-}
-
-function run(): void {
-  const port = process.env.PORT || 4000;
-  const server = app();
-
-  // Start the Node server
-  server.listen(port, () => {
-    console.log(`Node Express server listening on http://localhost:${port}`);
-  });
-}
-
-run();
+export default bootstrap;
